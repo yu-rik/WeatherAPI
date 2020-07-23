f19cc1acd613a0746b8f4e779e9f8ebb89f4ada1
Yurii Nosachenko <yurik_n@outlook.com>
2020-07-23 23:09:26 +0300

    Create interface

---
+++ WeatherAPI/Extensions/ViewController+Alert.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController+Alert.swift
+//  WeatherAPI
+//
+//  Created by yurik on 7/23/20.
+//  Copyright © 2020 yurik. All rights reserved.
+//
+
+import UIKit
+
+extension ViewController {
+    func presentSearchAlertController(withTitle title: String?, message: String?, style: UIAlertController.Style){
+        //создаем АлертКонтроллер и передаем значения из параметров метода
+        let ac = UIAlertController(title: title, message: message, preferredStyle: style)
+        //создаем текстовое поле в Алерт-Контроллере
+        ac.addTextField { (tf) in
+            let cities = ["Kyiv", "Lviv", "Odessa", "Kharkiv", "Chernigiv", "Yalta", "Donetsk"]
+            tf.placeholder = cities.randomElement()
+        }
+        //создаем кнопки
+        let search = UIAlertAction(title: "Search", style: .default) { (action) in
+            let textFD = ac.textFields?.first
+            guard let cityName = textFD?.text else {return}
+            if cityName != "" {
+                print("search info for the \(cityName)")
+            }
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        //добавляем кнопки Алерт-Контроллеру
+        ac.addAction(search)
+        ac.addAction(cancel)
+        
+        //показываем Алерт-Контроллер
+        present(ac, animated: true, completion: nil)
+    }
+}
---
+++ WeatherAPI/View Controllers/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  WeatherAPI
+//
+//  Created by yurik on 7/23/20.
+//  Copyright © 2020 yurik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var feelsLikeLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+       
+        presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
